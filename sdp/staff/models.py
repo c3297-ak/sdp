8a8ed0ea164ae5145a2f15e8d3b8c88b4b0c3640
Apoorva Katta <apoorvakatta@gmail.com>
2016-11-30 19:54:28 +0800

    resolve db

--- sdp/staff/models.py
+++ sdp/staff/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Staff(models.Model):
     username = models.CharField(max_length=8)
     password = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.username
