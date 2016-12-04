8e7d01f07ef3bfc08f5398b337478af21ac562b1
Apoorva Katta <apoorvakatta@gmail.com>
2016-12-04 14:43:34 +0800

    Revert "django_embed video for content in video component"
    This reverts commit c7d5fe7f14138e46c90716ce1567c98945f0a227.

--- sdp/sdp/settings.py
+++ sdp/sdp/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'courses',
     'staff',
-    'embed_video',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
